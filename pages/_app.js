272562f1451c145ea6d41aad37be485eaea88d29
Tian-que <1605206150@qq.com>
2022-02-26 00:03:49 +0800

    :bug: hidden scroll bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
 import 'regenerator-runtime/runtime';
+import '../styles/App.css'
 
 const colors = {
   brand: {
